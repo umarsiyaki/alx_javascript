1190f93c6e699f468bf365b37d0f293bb11a354c
umarsiyaki@gmail.com <umarsiyaki@gmail.com>
2023-09-22 16:39:23 +0100

    update file at zero index, 2 files added

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,11 @@
+// Using let to declare a variable for myObject
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+// Updating the value property
+myObject.value = 89;
+
+console.log(myObject);
---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+// Using let to declare variables and an array of strings
+let lines = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+
+// Using a for loop to print the lines
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
