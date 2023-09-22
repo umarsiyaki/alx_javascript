ce490f576f29784e28bd8e1614bd413d64888521
umarsiyaki@gmail.com <umarsiyaki@gmail.com>
2023-09-22 15:53:11 +0100

    good one dated

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,9 @@
-let c = "C is fun";
-let python = "Python is cool";
-let java = "javascript is amazing";
-console.log(c);
-console.log(python);
-console.log(java);
+// Using let to declare variables
+let line1 = "C is fun";
+let line2 = "Python is cool";
+let line3 = "JavaScript is amazing";
 
+// Printing the lines using console.log
+console.log(line1);
+console.log(line2);
+console.log(line3);
